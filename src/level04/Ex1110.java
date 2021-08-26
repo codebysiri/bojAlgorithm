5acb797f0bf93289d12bd19483dc996c5b9c43a1
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-08-26 20:45:47 +0900

    level04 - while

---
+++ src/level04/Ex1110.java
@@ -0,0 +1,21 @@
+package level04;
+
+import java.util.Scanner;
+
+public class Ex1110 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int COUNT = 0;
+        int NUM = N;
+
+        while (true) {
+            NUM = (NUM / 10 + NUM % 10) % 10 + NUM %10 * 10;
+            COUNT++;
+            if (N == NUM) {
+                break;
+            }
+        }
+        System.out.println(COUNT);
+    }
+}
